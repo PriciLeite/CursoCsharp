55e5dbb21e5c601b84693753c83c17a532d04baa
Priscila Leite <109990443+PriciLeite@users.noreply.github.com>
2023-02-08 10:12:13 -0300

    Criando a Classe Triangulo / Instanciando a Classe Triangulo no Program.cs

---
+++ CursoCsharp/Aula04.Classe.Atributos.Metodos.MetodosEstaticos/Triangulo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoCsharp.Aula04.Classe.Atributos.Metodos.MetodosEstaticos
+{
+    public class Triangulo
+    {
+        public double A ;
+        public double B;        //Atributos-> Letra Maiúscula
+        public double C;
+
+        
+
+    }
+}
--- CursoCsharp/Program.cs
+++ CursoCsharp/Program.cs
@@ -1,10 +1,100 @@
-﻿using System;
+﻿using CursoCsharp.Aula04.Classe.Atributos.Metodos.MetodosEstaticos;
+using System;
 using System.Globalization;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-       
+        static void Main(string[] args)
+        {
+            /*  Código sem aplicação da Classe Triângulo  
+             * 
+             *          double xA, xB, xC, yA, yB, yC;
+             *          
+            Console.WriteLine("Digite os lados do triângulo X");
+            double xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+             
+            Console.WriteLine("Digite os lados do triângulo Y");
+            double yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double pX = (xA+ xB + xC) / 2.0;
+            double areaX = Math.Sqrt(pX *(pX - xA)*(pX - xB)*(pX - xC));
+
+            double pY = (yA + yB + yC) / 2.0;
+            double areaY = Math.Sqrt(pY *(pY - yA)*(pY - yB)*(pY - yC));
+
+            Console.WriteLine("Área de X:{0}", areaX.ToString("F1", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y:{0}", areaY.ToString("F1", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Área de X Maior!");
+            }
+            else
+            {
+                Console.WriteLine("Área de Y Maior!");
+            }
+            */
+            
+            /* >>>>>>>>> Usando a Classe Triângulo:  */
+            
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Digite os lados do triângulo X");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite os lados do triângulo Y");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double pX = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(pX * (pX - x.A) * (pX - x.B) * (pX - x.C));
+
+            double pY = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(pY * (pY - y.A) * (pY - y.B) * (pY - y.C));
+
+            Console.WriteLine("Área de X:{0}", areaX.ToString("F1", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y:{0}", areaY.ToString("F1", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Área de X Maior!");
+            }
+            else
+            {
+                Console.WriteLine("Área de Y Maior!");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
     }
+
+
+
 }
